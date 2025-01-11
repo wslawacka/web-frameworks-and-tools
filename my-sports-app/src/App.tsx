02bcb9033610f418a141643c2497f60610854561
Weronika Sławacka <slawacka@wp.pl>
2025-01-11 17:56:24 +0100

    Clear mock database of players and implement localStorage for team management in App component. Enhance state management by persisting teams in localStorage and updating it on changes.

--- my-sports-app/src/App.tsx
+++ my-sports-app/src/App.tsx
@@ -1,12 +1,19 @@
 import { TeamType } from "./types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import TeamList from "./pages/TeamList";
 import TeamPage from "./pages/TeamPage";
 
 function App() {
-  const [teams, setTeams] = useState<TeamType[]>([]);
+  const [teams, setTeams] = useState<TeamType[]>(() => {
+    const teams = localStorage.getItem("teams");
+    return teams ? JSON.parse(teams) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("teams", JSON.stringify(teams));
+  }, [teams]);
 
   return (
     <Router>
