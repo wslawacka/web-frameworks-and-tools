c9fb824283d7754ea43d31990f8bc0269ea98687
Weronika Sławacka <slawacka@wp.pl>
2025-01-11 15:24:29 +0100

    Refactor Player component to enable inline editing of player attributes, including position, number, country, age, height, games, and goals. Update mock database with new player data types and values. Enhance CSS styles for player information display.

--- my-sports-app/src/components/Player.tsx
+++ my-sports-app/src/components/Player.tsx
@@ -1,4 +1,8 @@
+import axios from "axios";
+
 import { PlayerType } from "../types";
+
+import { useState } from "react";
 
 import "../styles/player.css";
 
@@ -18,49 +22,249 @@ interface PlayerProps {
 }
 
 function Player(props: PlayerProps) {
-  // create a player object from props
-  const player: PlayerType = {
-    id: props.id,
-    teamId: props.teamId,
-    name: props.name,
-    position: props.position,
-    number: props.number,
-    country: props.country,
-    age: props.age,
-    height: props.height,
-    weight: props.weight,
-    games: props.games,
-    goals: props.goals,
+  // state to track the editing field
+  const [editingField, setEditingField] = useState<string | null>(null);
+  // state to track the editing value
+  const [editingValue, setEditingValue] = useState<string | number | null>(
+    null
+  );
+
+  const [player, setPlayer] = useState<PlayerType>(props);
+
+  // function to handle the edit field
+  const handleEdit = (field: string, value: string | number) => {
+    setEditingField(field);
+    setEditingValue(value);
+  };
+
+  // function to handle the save field
+  const handleSave = async () => {
+    if (!editingField) return;
+
+    try {
+      await axios.patch(`http://localhost:3001/players/${props.id}`, {
+        [editingField]: editingValue,
+      });
+
+      setPlayer((prevPlayer) => ({
+        ...prevPlayer,
+        [editingField]: editingValue,
+      }));
+
+      setEditingField(null);
+      setEditingValue(null);
+    } catch (error) {
+      alert("Failed to update the player. Please try again.");
+    }
   };
 
   return (
     <div className="player">
       <h2>{player.name}</h2>
       <div className="player-info">
-        <p className="player-info-name">
+        <span className="player-info-item">
           Position: <span>{player.position}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "position" ? (
+            <div>
+              <input
+                placeholder="Position"
+                type="text"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("position", player.position)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Number: <span>{player.number}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "number" ? (
+            <div>
+              <input
+                placeholder="Number"
+                type="number"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("number", player.number)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Country: <span>{player.country}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "country" ? (
+            <div>
+              <input
+                placeholder="Country"
+                type="text"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("country", player.country)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Age: <span>{player.age}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "age" ? (
+            <div>
+              <input
+                placeholder="Age"
+                type="number"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("age", player.age)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Height: <span>{player.height}</span>
-        </p>
-        <p className="player-info-name">
-          Weight: <span>{player.weight}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "height" ? (
+            <div>
+              <input
+                placeholder="Height"
+                type="number"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("height", player.height)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Games: <span>{player.games}</span>
-        </p>
-        <p className="player-info-name">
+          {editingField === "games" ? (
+            <div>
+              <input
+                placeholder="Games"
+                type="number"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("games", player.games)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
+        <span className="player-info-item">
           Goals: <span>{player.goals}</span>
-        </p>
+          {editingField === "goals" ? (
+            <div>
+              <input
+                placeholder="Goals"
+                type="number"
+                value={editingValue ?? ""}
+                onChange={(e) => setEditingValue(e.target.value)}
+              />
+              <button className="save-button" onClick={handleSave}>
+                Save
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => setEditingField(null)}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="edit-button"
+              onClick={() => handleEdit("goals", player.goals)}
+            >
+              Edit
+            </button>
+          )}
+        </span>
       </div>
       <button
         className="delete-player-button"
